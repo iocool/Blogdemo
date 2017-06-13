4181be9e5838cb98fe3f03528230ff906da940e2
iocool <fannypanther@gmail.com>
2017-06-13 16:11:11 +0800

    增加日志功能

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ var flash = require('connect-flash');
 var config = require('config-lite');
 var routes = require('./routes');
 var pkg = require('./package');
+var winston = require('winston');
+var expressWinston = require('express-winston');
 
 var app = express();
 
@@ -57,8 +59,34 @@ app.use(function (req, res, next) {
     next();
 });
 
+// 正常请求的日志
+app.use(expressWinston.logger({
+  transports: [
+    new(winston.transports.Console)({
+      json: true,
+      colorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/success.log'
+    })
+  ]
+}));
+
 // 路由
 routes(app);
+
+// 错误的请求日志
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    }),
+    new winston.transports.File({
+      filename: 'logs/error.log'
+    })
+  ]
+}));
 
 // error 页面
 app.use(function (err, req, res, next) {
